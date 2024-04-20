0ed188a78fd118c2f83a3571d2e531ada6f5cb5c
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-04-20 17:20:08 +0530

    first commit

---
+++ hello-shoe-back-end/src/main/java/lk/ijse/helloshoebackend/HelloShoeBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShoeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloShoeBackEndApplication.class, args);
+	}
+
+}
---
+++ hello-shoe-back-end/src/test/java/lk/ijse/helloshoebackend/HelloShoeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoeBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
