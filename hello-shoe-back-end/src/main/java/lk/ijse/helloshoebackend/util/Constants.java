ced29aa907dc0fef17b99c1bad9ffe560c1d2c84
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-04-23 01:49:45 +0530

    add cons values

---
+++ hello-shoe-back-end/src/main/java/lk/ijse/helloshoebackend/util/Constants.java
@@ -0,0 +1,16 @@
+package lk.ijse.helloshoebackend.util;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-04-22
+ * @since 0.0.1
+ */
+public enum Constants {
+    REGULAR_USER,ADMIN_USER,
+
+    MAN, WOMAN,
+
+    ACTIVE, INACTIVE,
+
+    MARRIAGE, SINGLE
+}
