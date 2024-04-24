cc3ed1f463a0a632059655349891bc91638dac52
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-04-24 13:33:46 +0530

    change contact

--- hello-shoe-back-end/src/main/java/lk/ijse/helloshoebackend/entity/embedded/Contact.java
+++ hello-shoe-back-end/src/main/java/lk/ijse/helloshoebackend/entity/embedded/Contact.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Data
 @Embeddable
 public class Contact {
     @Column(length = 20 , unique = true)
