556ad1318165cadd19f30d7017451378d1530636
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-04-25 01:39:57 +0530

    change CustomerRepository

--- hello-shoe-back-end/src/main/java/lk/ijse/helloshoebackend/repository/CustomerRepository.java
+++ hello-shoe-back-end/src/main/java/lk/ijse/helloshoebackend/repository/CustomerRepository.java
@@ -1,7 +1,11 @@
 package lk.ijse.helloshoebackend.repository;
 
+import lk.ijse.helloshoebackend.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Dewmith Mihisara
@@ -9,5 +13,9 @@ import org.springframework.stereotype.Repository;
  * @since 0.0.1
  */
 @Repository
-public interface CustomerRepository extends JpaRepository<CustomerRepository, String>{
+public interface CustomerRepository extends JpaRepository<CustomerEntity, String>{
+    CustomerEntity findCustomerEntityByEmail(String email);
+    CustomerEntity findCustomerEntityByContact(String mobile);
+    List<CustomerEntity> findCustomerEntitiesByDob(Date dob);
+    boolean existsByEmailAndContact(String email, String contact);
 }
