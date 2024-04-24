4e981712edc8a7da3d7103bac14cbf5cee232263
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-04-24 13:34:14 +0530

    update SupplierRepository

--- hello-shoe-back-end/src/main/java/lk/ijse/helloshoebackend/repository/SupplierRepository.java
+++ hello-shoe-back-end/src/main/java/lk/ijse/helloshoebackend/repository/SupplierRepository.java
@@ -1,6 +1,7 @@
 package lk.ijse.helloshoebackend.repository;
 
 import lk.ijse.helloshoebackend.entity.SuppliersEntity;
+import lk.ijse.helloshoebackend.entity.embedded.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface SupplierRepository extends JpaRepository<SuppliersEntity, String> {
+    Boolean existsByContactAndEmail(Contact contact, String email);
 }
